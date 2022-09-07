2d3b15278eb8dd357bbbb3189c587c4e95e15935
Pranav <npv12@iitbbs.ac.in>
2022-09-07 04:02:22 +0200

    Revert "drm: Implement panel status check"
    This reverts commit 72853bdb4dd06d3034887734ec28c32e105055a2.
    
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- drivers/gpu/drm/drm_sysfs.c
+++ drivers/gpu/drm/drm_sysfs.c
@@ -1217,29 +1217,6 @@ static ssize_t oneplus_display_set_forcescreenfp(struct device *dev,
 	return count;
 }
 
-int oneplus_auth_status = 0;
-int oneplus_panel_status = 0;
-static ssize_t op_display_get_auth_status(struct device *dev,
-				struct device_attribute *attr, char *buf)
-{
-	return sprintf(buf, "%d\n", oneplus_auth_status);
-}
-
-static ssize_t op_display_set_auth_status(struct device *dev,
-				struct device_attribute *attr,
-				const char *buf, size_t count)
-{
-	sscanf(buf, "%d", &oneplus_auth_status);
-
-	return count;
-}
-
-static ssize_t op_display_get_power_status(struct device *dev,
-				struct device_attribute *attr, char *buf)
-{
-	return sprintf(buf, "%d\n", oneplus_panel_status);
-}
-
 static ssize_t op_display_get_dimlayer_enable(struct device *dev,
 					      struct device_attribute *attr,
 					      char *buf)
