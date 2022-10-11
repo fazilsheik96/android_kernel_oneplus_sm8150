252b9e0a5911b8355bd02180dfa51e61088d5eb2
John Galt <johngaltfirstrun@gmail.com>
2022-10-11 14:27:37 -0400

    dsi_panel: also override post wait on hbm off from fod
    Fixes e4cbd02d517944f4d88b82b7584d79202dd5a39d for depress sync,
    and some possible post unlock issues from it like from mistimed hbm off.

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -961,7 +961,7 @@ static void set_hbm_mode(struct work_struct *work)
 	switch (level) {
 	case 0:
 		if (!HBM_flag) {
-			dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_HBM_OFF);
+			__dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_HBM_OFF, true);
 			pr_err(
 				"When HBM OFF -->hbm_backight = %d panel->bl_config.bl_level =%d\n",
 				panel->hbm_backlight, panel->bl_config.bl_level);
@@ -5336,7 +5336,7 @@ int dsi_panel_set_hbm_mode(struct dsi_panel *panel, int level)
 			goto error;
 		} else {
 			HBM_flag = false;
-			rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_HBM_OFF);
+			rc = __dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_HBM_OFF, true);
 			pr_err("Send DSI_CMD_SET_HBM_OFF cmds.\n");
 			pr_err("hbm_backight = %d, panel->bl_config.bl_level = %d\n",
 			       panel->hbm_backlight, panel->bl_config.bl_level);
